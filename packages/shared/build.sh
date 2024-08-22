8512ebfc721ee4e5cb8341eae373a67a60c74b7f
jackie19 <jackie198512@126.com>
2024-08-22 11:49:55 +0800

    fix: setup prisma client generation for langfuse

--- packages/shared/build.sh
+++ packages/shared/build.sh
@@ -1,6 +1,11 @@
 
 
 # Generate the prisma-client-for-langfuse-linux
+# remove workspace from devDependencies from package.json
+# open git bash
+# cd /d/work/fork/langfuse/packages/shared/
+# sh build.sh
+
 
  docker build -t shared .
  docker create --name shared-ins shared
