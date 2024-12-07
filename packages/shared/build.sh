c7cf9379b2eebf1b8b0bea7c0b7b1584000a84f9
jackie19 <jackie198512@126.com>
2024-12-07 19:44:14 +0800

    fix: setup prisma client generation for langfuse

--- packages/shared/build.sh
+++ packages/shared/build.sh
@@ -6,6 +6,7 @@
 # cd /d/work/fork/langfuse/packages/shared/
 # sh build.sh
 
+rm -rf ./tmp/*
 
  docker build -t shared .
  docker create --name shared-ins shared
@@ -14,6 +15,8 @@
  docker rm shared-ins
  docker image rm shared -f
 
-
+ls ./tmp
+rm -rf  /d/work/prisma-client-for-langfuse-linux/*
+cp -r ./tmp/prisma-client-for-langfuse-linux/* /d/work/prisma-client-for-langfuse-linux/
 # for windows, change output to x64
 # run `npx prisma generate`
